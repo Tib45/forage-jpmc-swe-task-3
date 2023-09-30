77ac08c8ae1905571d722c57d5af1068753d4895
Tiburce Descout <tiburce.descout@grenoble-inp.org>
2023-09-30 16:19:39 +0200

    part 3

--- src/DataManipulator.ts
+++ src/DataManipulator.ts
@@ -1,20 +1,46 @@
-import { ServerRespond } from './DataStreamer';
+import { ServerRespond } from "./DataStreamer";
 
 export interface Row {
-  stock: string,
-  top_ask_price: number,
-  timestamp: Date,
+  price_abc: number;
+  price_def: number;
+  ratio: number;
+  upper_bound: number;
+  lower_bound: number;
+  trigger_alert: number | undefined;
+  timestamp: Date;
 }
-
 
 export class DataManipulator {
   static generateRow(serverResponds: ServerRespond[]) {
-    return serverResponds.map((el: any) => {
-      return {
-        stock: el.stock,
-        top_ask_price: el.top_ask && el.top_ask.price || 0,
-        timestamp: el.timestamp,
-      };
-    })
+    // Get API results:
+    const abc = serverResponds[0];
+    const def = serverResponds[1];
+
+    // Bound values:
+    const boundThr = 0.05;
+    const upperBound = 1 + boundThr;
+    const lowerBound = 1 - boundThr;
+
+    // Compute prices, ratio, trigger alert and timestamp:
+    const priceABC = (abc.top_ask.price + abc.top_bid.price) / 2;
+    const priceDEF = (def.top_ask.price + def.top_bid.price) / 2;
+    const ratio = priceABC / priceDEF;
+    const triggerAlet =
+      ratio > upperBound || ratio < lowerBound ? ratio : undefined;
+
+    const timestamp =
+      serverResponds[0].timestamp > serverResponds[1].timestamp
+        ? serverResponds[0].timestamp
+        : serverResponds[1].timestamp;
+
+    return {
+      price_abc: priceABC,
+      price_def: priceDEF,
+      ratio: ratio,
+      timestamp: timestamp,
+      upper_bound: upperBound,
+      lower_bound: lowerBound,
+      trigger_alert: triggerAlet,
+    };
   }
 }
